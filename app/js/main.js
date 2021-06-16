f69f79161b7f217bf09064b1cc8fc5430d5fa36c
vladian95 <kvankvan1995@gmail.com>
2021-06-16 21:01:20 +0200

    added tabs

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,13 @@
 $(function(){
 
-
+    $('.tabs__inner .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.tabs__inner').find('.tab-item').removeClass('active-tab').hide();
+        $('.tabs__inner .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
+    });
 
     
 });
